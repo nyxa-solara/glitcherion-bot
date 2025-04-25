50173f6b64f1f557535b583cb997808a66fb8aaa
boreealis <meriri.daveite@gmail.com>
2025-04-24 22:51:19 -0400

    Add files via upload
    Primer latido de Glitcherion 🛸

---
+++ main.py
@@ -0,0 +1,17 @@
+
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import logging
+
+logging.basicConfig(level=logging.INFO)
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("🌌 Saludos terrícola Amanda. Yo soy Glitcherion. Pronto, viajarás.")
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
+
+if __name__ == "__main__":
+    app.run_polling()
